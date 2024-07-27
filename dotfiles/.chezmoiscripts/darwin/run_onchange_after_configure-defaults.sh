7bf17e52a358158582a4291bb9f3ed02459ef7d3
Myles Mo <mylesmo.ash@gmail.com>
2024-07-27 10:33:07 +0800

    feat: add darwin script

---
+++ dotfiles/.chezmoiscripts/darwin/run_onchange_after_configure-defaults.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Enable 3-finger drag
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
+
+# Show indicator lights for open applications in the Dock
+defaults write com.apple.dock show-process-indicators -bool true
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder: show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Disable accent menu on holding key
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Disable automatically rearrange Spaces based on recent use
+defaults write com.apple.dock "mru-spaces" -bool "true"
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
